6ee6f30c37378d24fa61846ae9736afb9ecd8577
eee555 <50390200+eee555@users.noreply.github.com>
2023-05-04 17:53:50 +0800

    fix: a small mistake

--- src/metaminesweeper_checksum.py
+++ src/metaminesweeper_checksum.py
@@ -3,7 +3,7 @@
 # 校验和的守卫，调试用。真实打包用的代码不开源
 class ChecksumGuard():
     def get_checksum(self, data):
-        return b'\0'*32
+        return b'\7'*32
     def valid_checksum(self, data, checksum):
         return False
 
